31616341985eebbcbf453e63b76d9361a6345295
francobuena <37284096+francobuena@users.noreply.github.com>
2025-03-22 14:57:37 +1100

    setup network layer for project, create model for responses and requests, create initial view model, test endpoints (#1)

---
+++ Bank-Techinical-Challenge/Bank-Techinical-Challenge/Network/Endpoint.swift
@@ -0,0 +1,62 @@
+//
+//  Endpoint.swift
+//  Bank-Techinical-Challenge
+//
+//  Created by Buena, Franco on 22/3/2025.
+//
+
+import Foundation
+
+enum Endpoint {
+    private struct Constants {
+        static let baseURLString = "https://api-sandbox.starlingbank.com/"
+        static let basePath = "api/v2/"
+    }
+    
+    case accounts
+    case transactions(accountId: String, categoryId: String)
+    case savingsGoals(accountId: String, method: String, request: Data?)
+    
+    private func path() -> String {
+        switch self {
+        case .accounts:
+            return "accounts"
+        case .transactions(let accountId, let categoryId):
+            // TODO: we need to move this changesSince parameter somewhere
+            return "feed/account/\(accountId)/category/\(categoryId)?changesSince=2025-03-21T00:00:00.000Z"
+        case .savingsGoals(let accountId, _, _):
+            return "account/\(accountId)/savings-goals"
+        }
+    }
+    
+    private var httpMethod: String {
+        switch self {
+        case .accounts, .transactions:
+            return "GET"
+        case .savingsGoals(_, let method, _):
+            return method
+        }
+    }
+    
+    func request() throws -> URLRequest {
+        guard let url = URL(string: "\(Constants.baseURLString)\(Constants.basePath)\(path())") else {
+            throw NetworkError.malformedURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod
+        
+        switch self {
+        case .savingsGoals(_, _, let body):
+            if let body {
+                request.httpBody = body
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+        default:
+            break
+        }
+        
+        return request
+    }
+}
+
