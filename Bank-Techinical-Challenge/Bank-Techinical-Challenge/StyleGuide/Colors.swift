8fd0e8d5e1b4eaee13d1bc70dfe79a01462ec9c8
francobuena <francobuena@outlook.com>
2025-03-24 11:20:42 +1100

    add brand colors that works in light and dark mode

---
+++ Bank-Techinical-Challenge/Bank-Techinical-Challenge/StyleGuide/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Bank-Techinical-Challenge
+//
+//  Created by Buena, Franco on 24/3/2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct Colors {
+    static let topSectionColor: Color = Color("TopSectionColor")
+    static let transactionCellColor: Color = Color("TransactionCell")
+    static let transactionBackgroundColor: Color = Color("TransactionBackground")
+    static let currencyFontColor: Color = Color("CurrencyFontColor")
+}
